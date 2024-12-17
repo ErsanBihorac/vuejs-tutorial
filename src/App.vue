501717292788d050e76c54023b63cd52d77b121d
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-12-17 02:06:35 +0100

    modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,55 @@
 <template>
-  <h2 v-for="actor in actors" :key="actor.name">
-    <h2>{{ actor.name }}</h2>
-    <h3 v-for="movie in actor.movies" :key="movie">{{ movie }}</h3>
-  </h2>
+  <div>
+    <pre>
+      {{ JSON.stringify(formValues, null, 2) }}
+    </pre>
+  </div>
+  <form>
+    <div>
+      <label for="name">Name</label>
+      <input type="text" id="name" v-model.trim.lazy="formValues.name">
+    </div>
+
+    <div>
+      <label for="country">country</label>
+      <select id="country" v-model="formValues.country">
+        <option value="">select a country</option>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="job-location">Job Location</label>
+      <select id="job-location" multiple v-model="formValues.jobLocation">
+        <!-- <option value="">select a country</option> -->
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="singapore">Singapore</option>
+      </select>
+    </div>
+
+    <div>
+      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
+      <label for="remoteWork">Open to remote work?</label>
+    </div>
+
+    <div>
+      <label>Skill set</label>
+      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
+      <label for="html">HTML</label>
+      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
+      <label for="css">CSS</label>
+      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet">
+      <label for="javascript">JavaScript</label>
+    </div>
+
+    <div>
+      <label for="age">Age</label>
+      <input type="number" id="age" v-model.number="formValues.age">
+    </div>
+  </form>
 </template>
 
 <script>
@@ -10,22 +57,15 @@ export default {
   name: 'App',
   data() {
     return {
-      names: ['Bruce', 'Clark', 'Diana'],
-      fullNames: [
-        { first: 'Bruce', last: 'Wayne' },
-        { first: 'Clark', last: 'Kent' },
-        { first: 'Princess', last: 'Diana' },
-      ],
-      actors: [
-        {
-          name: 'Christian bale',
-          movies: ['Batman', 'The Prestige'],
-        },
-        {
-          name: 'Di Caprio',
-          movies: ['Titatnic', 'Inception'],
-        }
-      ]
+      formValues: {
+        name: '',
+        country: '',
+        jobLocation: [],
+        remoteWork: 'no',
+        skillSet: [],
+        yearsOfExperience: '',
+        age: null,
+      }
     }
   }
 }
@@ -36,24 +76,34 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
 
-.underline {
-  text-decoration: underline;
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
 }
 
-.promoted {
-  font-style: italic;
+input+label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
 }
 
-.new {
-  color: olivedrab;
-}
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
 
-.sold-out {
-  color: red;
 }
 </style>
