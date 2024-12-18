b4429610be68700f2b951f31ccd7a0829e76558a
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-12-18 03:41:07 +0100

    deep/immediate watchers

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,16 @@
 <template>
+  <h2>Volume Tracker (0-20)</h2>
+  <h3>Current Volume - {{ volume }}</h3>
   <div>
-    <pre>
-      {{ JSON.stringify(formValues, null, 2) }}
-    </pre>
+    <button @click="volume += 2">Increase</button>
+    <button @click="volume -= 2">Decrease</button>
   </div>
-  <form>
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model.trim.lazy="formValues.name">
-    </div>
-
-    <div>
-      <label for="country">country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">select a country</option>
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-    <div>
-      <label for="job-location">Job Location</label>
-      <select id="job-location" multiple v-model="formValues.jobLocation">
-        <!-- <option value="">select a country</option> -->
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-    <div>
-      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no">
-      <label for="remoteWork">Open to remote work?</label>
-    </div>
-
-    <div>
-      <label>Skill set</label>
-      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet">
-      <label for="html">HTML</label>
-      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet">
-      <label for="css">CSS</label>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet">
-      <label for="javascript">JavaScript</label>
-    </div>
-
-    <div>
-      <label for="age">Age</label>
-      <input type="number" id="age" v-model.number="formValues.age">
-    </div>
-  </form>
+  <input type="text" v-model="movie">
+  <input type="text" v-model="movieInfo.title">
+  <input type="text" v-model="movieInfo.actor">
+  <div>
+    <button @click="movieList = movieList.concat(['Wonder Woman'])">Add Movie</button>
+  </div>
 </template>
 
 <script>
@@ -57,15 +18,44 @@ export default {
   name: 'App',
   data() {
     return {
-      formValues: {
-        name: '',
-        country: '',
-        jobLocation: [],
-        remoteWork: 'no',
-        skillSet: [],
-        yearsOfExperience: '',
-        age: null,
+      volume: 0,
+      movie: 'Batman',
+      movieInfo: {
+        title: '',
+        actor: ''
+      },
+      movieList: ['Batman', 'Superman']
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+  watch: {
+    volume(newValue, oldValue) {
+      if (newValue > oldValue && newValue === 16) {
+        alert('Listening to a high volume...')
       }
+    },
+    movie: {
+      handler(newValue) {
+        console.log(`Calling API with movie name = ${newValue}`)
+      },
+      immediate: true,
+    },
+    movieInfo: {
+      handler(newValue) {
+        console.log(`Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`)
+      },
+      deep: true,
+    },
+    movieList: {
+      handler(newValue) {
+        console.log(`updated list ${newValue}`)
+      },
+      // deep: true,
     }
   }
 }
@@ -79,31 +69,5 @@ export default {
   /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
-}
-
-label {
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
-
-input+label {
-  font-weight: bold;
-  display: inline-flex;
-  margin-right: 20px;
-}
-
-input[type='text'],
-textarea,
-select {
-  display: block;
-  width: 400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-
 }
 </style>
